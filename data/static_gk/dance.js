ae8dd7b4d7d7939505081426a6a92b23cca11c7e
navi-git-15 <navkiratsingh1019@gmail.com>
2025-10-23 11:43:17 +0530

    Create dance.js

---
+++ data/static_gk/dance.js
@@ -0,0 +1,82 @@
+window.quizData = [
+  {
+    question:
+      "Indian classical dance techniques like Rasa and Bhava trace their origins to —",
+    options: [
+      "Abhinaya Darpana",
+      "Bharatmuni's Natyashastra",
+      "Sangeet Ratnakar",
+      "Natya Veda",
+    ],
+    answer: "Bharatmuni's Natyashastra",
+  },
+
+  {
+    question:
+      "Lavani, Powada, Koli, Waghya Murali and Dhangari Gaja are folk dances from —",
+    options: ["Gujarat", "Maharashtra", "Karnataka", "Madhya Pradesh"],
+    answer: "Maharashtra",
+  },
+
+  {
+    question: "The one-act play of Sattriya is called —",
+    options: ["Yakshagana", "Ankiya Nat", "Therukoothu", "Jatra"],
+    answer: "Ankiya Nat",
+  },
+
+  {
+    question:
+      "The origin of most of the classical dance forms of India is from —",
+    options: [
+      "Natya Shastra",
+      "Rig Veda",
+      "Silappadikaram",
+      "Sangeet Ratnakar",
+    ],
+    answer: "Natya Shastra",
+  },
+
+  {
+    question: "The dance form of Goa which is known as 'Warrior Dance' is —",
+    options: ["Ghode Modni", "Dhalo", "Fugdi", "Dekhnni"],
+    answer: "Ghode Modni",
+  },
+
+  {
+    question: "Manipuri dance is based on Rasleela themes of —",
+    options: [
+      "Shiva and Parvati",
+      "Radha and Krishna",
+      "Rama and Sita",
+      "Vishnu and Lakshmi",
+    ],
+    answer: "Radha and Krishna",
+  },
+
+  {
+    question:
+      "Sattriya (Assam) was recognised as a Classical dance by the Sangeet Natak Akademi in —",
+    options: ["1998", "2000", "2002", "2004"],
+    answer: "2000",
+  },
+
+  {
+    question:
+      "UNESCO recognized 'Mudiyettu,' a ritual-drama of Kerala, as an 'intangible cultural heritage of humanity' in —",
+    options: ["2008", "2009", "2010", "2011"],
+    answer: "2010",
+  },
+
+  {
+    question: "Thullal dance originated from —",
+    options: ["Tamil Nadu", "Kerala", "Andhra Pradesh", "Karnataka"],
+    answer: "Kerala",
+  },
+
+  {
+    question:
+      "The 'Ka Shad Mastieh' or victory dance of the Khasi tribe is from —",
+    options: ["Nagaland", "Assam", "Manipur", "Meghalaya"],
+    answer: "Meghalaya",
+  },
+];
